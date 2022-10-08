177ff1877b3b98a08a921be09cb5a34bfc97df1d
Danil Zhikharev <106320031+Danil-Zhikharev@users.noreply.github.com>
2022-10-09 00:02:58 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        int current_balance = 100;
+        int currentBalance = 100;
         int amount = 1500;
         int bonus;
-        if(amount >= 1000) {
+        if(amount > 1000) {
             bonus = amount / 100;
         } else {
             bonus = 0;
         }
-        int total = current_balance + amount + bonus;
+        int total = currentBalance + amount + bonus;
         System.out.println("Ваш бонус: " + bonus + " руб");
         System.out.println("Итоговая сумма: " + total + " руб");
     }
